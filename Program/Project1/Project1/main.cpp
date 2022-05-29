9b3cf4928de0458646fba40c2a56c72cfb039e19
tomislav.vidovic <tomislav.vidovic@fer.hr>
2022-05-29 18:21:01 +0200

    Nadodane strukture rubova i vrhova

--- Program/Project1/Project1/main.cpp
+++ Program/Project1/Project1/main.cpp
@@ -31,28 +31,114 @@ struct Node
 	string value;
 };
 
+struct NewNodes
+{
+	int id;
+	string value;
+};
+
+struct Edge
+{
+	int previousNode;
+	int nextNode;
+};
+
+vector<NewNodes> Nodes;
+vector<Edge> Edges;
+
 vector<Node> Graph;
 //vector<char> LinearLine;
 
-/*void LinearGraphParsing() {
-	string line;
-	ifstream graphFile("GraphFiles\\ref10000_linear.gfa");
-	if (graphFile.is_open())
-	{
-		while (getline(graphFile, line)) {
-			if(line[)
-			cout << line << '\n';
-		}
-		graphFile.close();
+//void LinearGraphParsing() {
+//	string line;
+//	ifstream graphFile("GraphFiles\\ref10000_linear.gfa");
+//	if (graphFile.is_open())
+//	{
+//		while (getline(graphFile, line)) {
+//			for (int i = 0; i < line.size(); i++)
+//			{
+//				if (isalpha(line[i]))
+//				{
+//					Node newNode = 
+//				}
+//			}
+//		}
+//		graphFile.close();
+//
+//	}
+//	else cout << "File oppening error";
+//
+//	for (int i = 0; i <= line.length(); i++)
+//	{
+//		cout << i + 1 << line[i] << "\n";
+//	}
+//}
 
-	}
-	else cout << "File oppening error";
-
-	for (int i = 0; i <= line.length(); i++)
-	{
-		cout << i + 1 << line[i] << "\n";
-	}
-}*/
+// Backup code
+//
+//
+//if (graphFile.is_open())
+//{
+//	while (getline(graphFile, tmp)) {
+//		if (tmp[0] == NODE_CHAR)
+//		{
+//			Node newNode;
+//			string id = "";
+//			int i = NODE_ID_LOCATION;
+//			while (isdigit(tmp[i]))
+//			{
+//				id += tmp[i];
+//				i++;
+//			}
+//			newNode.id = stoi(id);
+//			i = NODE_VALUE_STARTING_LOCATION + id.length() - 1;
+//			id = "";
+//			while (isalpha(tmp[i]))
+//			{
+//				id += tmp[i];
+//				i++;
+//			}
+//			newNode.value = id;
+//			Graph.push_back(newNode);
+//		}
+//		else if (tmp[0] == EDGE_CHAR)
+//		{
+//			string id = "";
+//			int i = EDGE_FIRST_NODE_STARTING_POSITION;
+//
+//			while (isdigit(tmp[i]))
+//			{
+//				id += tmp[i];
+//				i++;
+//			}
+//			int firstNode = stoi(id) - 1;
+//			int lengthFirstElement = id.length();
+//
+//			//Ovdje treba provjeriti kaj se dogada, jer postoji nekad i prije M-a da je minus, kako onda ide komplement?
+//			i = EDGE_APPENDING_FIRST_STARTING_POSITION + lengthFirstElement - 1;
+//			i = EDGE_LAST_NODE_STARTING_POSITION + lengthFirstElement - 1;
+//			id = "";
+//			while (isdigit(tmp[i]))
+//			{
+//				id += tmp[i];
+//				i++;
+//			}
+//			//cout << id<< "\n";
+//			Graph[firstNode].nextNode.push_back(stoi(id));
+//		}
+//		tmp = "";
+//	}
+//	graphFile.close();
+//	for (int i = 0; i < Graph.size(); i++)
+//	{
+//		cout << Graph[i].id << "\t" << Graph[i].value << "\n";
+//		for (int j = 0; j < Graph[i].nextNode.size(); j++)
+//			cout << Graph[i].nextNode[j] << "\n";
+//		cout << "\n";
+//	}
+//}
+//
+//
 
 int main() {
 	string line;
@@ -63,7 +149,7 @@ int main() {
 		while (getline(graphFile, tmp)) {
 			if (tmp[0] == NODE_CHAR)
 			{
-				Node newNode;
+				NewNodes newNode;
 				string id = "";
 				int i = NODE_ID_LOCATION;
 				while (isdigit(tmp[i]))
@@ -80,19 +166,22 @@ int main() {
 					i++;
 				}
 				newNode.value = id;
-				Graph.push_back(newNode);
+				Nodes.push_back(newNode);
 			}
 			else if (tmp[0] == EDGE_CHAR)
 			{
 				string id = "";
 				int i = EDGE_FIRST_NODE_STARTING_POSITION;
+				Edge edge;
 
 				while (isdigit(tmp[i]))
 				{
 					id += tmp[i];
 					i++;
 				}
-				int firstNode = stoi(id)-1;
+
+				edge.previousNode = stoi(id) - 1;
+				//int firstNode = stoi(id)-1;
 				int lengthFirstElement = id.length();
 
 				//Ovdje treba provjeriti kaj se dogada, jer postoji nekad i prije M-a da je minus, kako onda ide komplement?
@@ -105,18 +194,20 @@ int main() {
 					i++;
 				}
 				//cout << id<< "\n";
-				Graph[firstNode].nextNode.push_back(stoi(id));
+				edge.nextNode = stoi(id) - 1;
+				Edges.push_back(edge);
+				//Graph[firstNode].nextNode.push_back(stoi(id));
 			}
 			tmp = "";
 		}
 		graphFile.close();
-		for (int i = 0; i < Graph.size(); i++)
+		/*for (int i = 0; i < Graph.size(); i++)
 		{
 			cout << Graph[i].id << "\t" << Graph[i].value << "\n";
 			for (int j = 0; j < Graph[i].nextNode.size(); j++)
 				cout << Graph[i].nextNode[j] << "\n";
 			cout << "\n";
-		}
+		}*/
 	}
 	else cout << "File oppening error";
 
